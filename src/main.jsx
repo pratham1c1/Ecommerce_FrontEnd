448a7e1ca91c01a209e84cf0fa37268cc13bdec4
pratham1c1 <pratha.1c1@gmail.com>
2026-07-13 22:44:44 +0530

    Adding basepath for main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-import App from './App'
-import './styles/global.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><HashRouter><App /></HashRouter></React.StrictMode>)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter basename="/Ecommerce_FrontEnd/">
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+);
