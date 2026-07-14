7e5dfa2b18d5b53b7b6f602e18449944027bf14d
pratham1c1 <pratha.1c1@gmail.com>
2026-07-14 05:53:52 +0530

    remove basename from main

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="/Ecommerce_FrontEnd/">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
